ebf6dfd53cc17b3594413fd980470db6add3bcb2
Brady Vercher <brady@blazersix.com>
2013-08-14 19:03:54 -0700

    Bump version to 1.2.4

--- better-internal-link-search.php
+++ better-internal-link-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: Better Internal Link Search
  * Plugin URI: http://wordpress.org/extend/plugins/better-internal-link-search/
  * Description: Improve the internal link popup functionality with time saving enhancements and features.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Author: Blazer Six
  * Author URI: http://www.blazersix.com/
  * License: GPL-2.0+
