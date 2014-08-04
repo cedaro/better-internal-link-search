2ddc3e6c0414fc981c076474c320a9c0c85175ef
Brady Vercher <brady@blazersix.com>
2014-08-04 10:05:23 -0700

    Refactor internal link manager script and update to use the 'wplink-open' event restored in WordPress 4.0.

--- js/internal-link-manager.js
+++ js/internal-link-manager.js
@@ -1,75 +1,147 @@
-/*global BilsSettings:true, tinyMCE:true, wpLink:true */
+/*global _:false, BilsSettings:true, QTags:false, tinymce:false, wpLink:true */
 
-(function(window, $, undefined) {
+(function( window, $, _, undefined ) {
 	'use strict';
 
-	jQuery(function($) {
-		var $searchField = $('#search-field').width(200),
-			timeout;
+	var Editor, Manager;
 
-		$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-			if ( 'data' in options && -1 !== options.data.indexOf('action=wp-link-ajax') && -1 !== options.data.indexOf('search=') ) {
-				// Abort the request if it's just for resetting the river.
-				if ( -1 !== options.data.indexOf('better-internal-link-search-reset-river-flag') ) {
-					jqXHR.abort();
-				}
+	Editor = {
+		/**
+		 * Retrieve text selected in the editor.
+		 *
+		 * @global QTags
+		 * @global wpActiveEditor
+		 *
+		 * @return {string} Selected text.
+		 */
+		getSelection: function() {
+			var ed = this.getTinyMce(),
+				selection = '',
+				wpActiveEditor = window.wpActiveEditor,
+				end, start;
 
-				// Reset the search field to a single dash.
-				if ( -1 !== options.data.indexOf('search=-help') ) {
-					$('#search-field').val('-');
+			if ( false === ed ) {
+				return false;
+			}
+
+			if ( ed && ! ed.isHidden() ) {
+				selection = ed.selection.getContent();
+			} else if ( 'undefined' !== typeof QTags ) {
+				ed = QTags.getInstance( wpActiveEditor );
+				start = ed.canvas.selectionStart;
+				end = ed.canvas.selectionEnd;
+
+				if ( end - start > 0 ) {
+					selection = ed.canvas.value.substring( start, end );
 				}
 			}
-		});
 
-		$('#wp-link').on('wpdialogbeforeopen', function() {
-			var searchTerm = '-',
-				editor;
+			return selection;
+		},
 
-			// Don't mind me, just debouncing, yo.
-			$searchField.off('keyup').on('keyup.bils', function() {
-				var self = this,
-					$self = $(this);
+		/**
+		 * Retrieve the current instance of TinyMCE.
+		 *
+		 * @global QTags
+		 * @global tinymce
+		 * @global wpActiveEditor
+		 *
+		 * @return {Object|bool} TinyMCE instance, null, or false.
+		 */
+		getTinyMce: function() {
+			var mce = 'undefined' !== typeof tinymce,
+				qt = 'undefined' !== typeof QTags,
+				wpActiveEditor = window.wpActiveEditor,
+				ed;
 
-				clearTimeout(timeout);
-				timeout = setTimeout( function() {
-					if ( '-' === $self.val() || 0 === $self.val().indexOf('-help') ) {
-						// Ugly hack to reset the river...
-						$self.val('better-internal-link-search-reset-river-flag');
-						wpLink.searchInternalLinks.apply( self );
-						// And then bypass the three character minimum requirement.
-						$self.val('-help');
-					}
-
-					wpLink.searchInternalLinks.apply( self );
-				}, 500 );
-			});
-
-			// Determine what text is selected in the editor.
-			if ( 'undefined' !== typeof tinyMCE && ( editor = tinyMCE.activeEditor ) && ! editor.isHidden() ) {
-
-				var a = editor.dom.getParent(editor.selection.getNode(), 'A');
-				if ( null === a ) {
-					searchTerm = editor.selection.getContent({ format: 'text' });
+			if ( ! wpActiveEditor ) {
+				if ( mce && tinymce.activeEditor ) {
+					ed = tinymce.activeEditor;
+					wpActiveEditor = window.wpActiveEditor = ed.id;
+				} else if ( ! qt ) {
+					return false;
+				}
+			} else if ( mce ) {
+				if ( tinymce.activeEditor && ( 'mce_fullscreen' === tinymce.activeEditor.id || 'wp_mce_fullscreen' === tinymce.activeEditor.id ) ) {
+					ed = tinymce.activeEditor;
 				} else {
-					searchTerm = $(a).text();
-				}
-			} else {
-				var start = wpLink.textarea.selectionStart,
-					end = wpLink.textarea.selectionEnd;
-
-				if ( 0 < end-start ) {
-					searchTerm = wpLink.textarea.value.substring(start, end);
+					ed = tinymce.get( wpActiveEditor );
 				}
 			}
+
+			return ed;
+		}
+	};
+
+	Manager = {
+		$searchField: $(),
+
+		/**
+		 * Initialize the wpLink manager.
+		 */
+		init: function() {
+			this.$searchField = $( '#search-field' ).width( 200 );
+
+			// Proxy the internal link search method.
+			this._wpLinkSearchInternalLinks = wpLink.searchInternalLinks;
+			wpLink.searchInternalLinks = this.searchInternalLinks;
+
+			_.bindAll( this, 'searchSelection' );
+			$( '#wp-link' ).on( 'wpdialogbeforeopen', this.searchSelection );
+			$( document ).on( 'wplink-open', this.searchSelection );
+		},
+
+		/**
+		 * Search for internal links.
+		 */
+		searchInternalLinks: function() {
+			if ( '-' === Manager.$searchField.val() || 0 === Manager.$searchField.val().indexOf( '-help' ) ) {
+				// Ugly hack to reset the river...
+				Manager.$searchField.val( 'better-internal-link-search-reset-river-flag' );
+				Manager._wpLinkSearchInternalLinks.apply( this );
+
+				// And then bypass the three character minimum requirement.
+				Manager.$searchField.val( '-help' );
+			}
+
+			Manager._wpLinkSearchInternalLinks.apply( this );
+		},
+
+		/**
+		 * Trigger a search for the text selected in the active editor.
+		 */
+		searchSelection: function() {
+			var searchTerm = Editor.getSelection();
 
 			// Strip any html to get a clean search term.
-			if ( -1 !== searchTerm.indexOf('<') ) {
-				searchTerm = searchTerm.replace(/(<[^>]+>)/ig,'');
+			if ( -1 !== searchTerm.indexOf( '<' ) ) {
+				searchTerm = searchTerm.replace( /(<[^>]+>)/ig, '' );
 			}
 
 			if ( 'yes' === BilsSettings.automatically_search_selection && searchTerm.length ) {
-				$searchField.val( $.trim(searchTerm) ).keyup();
+				this.$searchField.val( $.trim( searchTerm ) ).keyup();
 			}
-		});
+		}
+	};
+
+	/**
+	 * Filter AJAX requests.
+	 */
+	$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+		if ( 'data' in options && -1 !== options.data.indexOf( 'action=wp-link-ajax' ) && -1 !== options.data.indexOf( 'search=' ) ) {
+			// Abort the request if it's just for resetting the river.
+			if ( -1 !== options.data.indexOf( 'better-internal-link-search-reset-river-flag' ) ) {
+				jqXHR.abort();
+			}
+
+			// Reset the search field to a single dash.
+			if ( -1 !== options.data.indexOf( 'search=-help' ) ) {
+				Manager.$searchField.val( '-' );
+			}
+		}
 	});
-})(this, jQuery);
+
+	$( document ).ready(function() {
+		Manager.init();
+	});
+})( this, jQuery, _ );
