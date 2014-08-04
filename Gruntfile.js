732ace1a514ea0f0a35b8a1f58f96857ae7b5630
Brady Vercher <brady@blazersix.com>
2014-08-04 10:28:12 -0700

    Update Grunt tasks and plugins.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,12 @@
-module.exports = function( grunt ) {
+/*jshint node:true */
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+module.exports = function( grunt ) {
+	'use strict';
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
 
 	grunt.initConfig( {
 
-		pkg: grunt.file.readJSON( 'package.json' ),
-
-		/**
-		 * Check JavaScript for errors and warnings.
-		 */
 		jshint: {
 			options: {
 				jshintrc: '.jshintrc'
@@ -22,10 +18,28 @@ module.exports = function( grunt ) {
 			]
 		},
 
-		/**
-		 * Minify JavaScript source files.
-		 */
-		/*
+		makepot: {
+			plugin: {
+				options: {
+					mainFile: 'better-internal-link-search.php',
+					potHeaders: {
+						'report-msgid-bugs-to': 'http://wordpress.org/support/plugin/better-internal-link-search',
+						'language': 'en',
+						'plural-forms': 'nplurals=2; plural=(n != 1);',
+						'x-poedit-basepath': '../',
+						'x-poedit-bookmarks': '',
+						'x-poedit-country': 'United States',
+						'x-poedit-keywordslist': '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;',
+						'x-poedit-searchpath-0': '.',
+						'x-poedit-sourcecharset': 'utf-8',
+						'x-textdomain-support': 'yes'
+					},
+					type: 'wp-plugin',
+					updateTimestamp: false
+				}
+			}
+		},
+
 		uglify: {
 			dist: {
 				files: [
@@ -39,23 +53,16 @@ module.exports = function( grunt ) {
 				]
 			}
 		},
-		*/
 
-		/**
-		 * Watch sources files and compile when they're changed.
-		 */
 		watch: {
 			js: {
-				files: [ '<%= jshint.all %>' ],
-				tasks: [ 'jshint', 'uglify' ]
+				files: ['<%= jshint.all %>'],
+				tasks: ['jshint']
 			}
 		}
 
 	} );
 
-	/**
-	 * Register the default task.
-	 */
-	grunt.registerTask( 'default', [ 'jshint', 'watch' ] );
+	grunt.registerTask( 'default', ['jshint', 'watch']);
 
 };
