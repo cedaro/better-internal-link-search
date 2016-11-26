c2d16b4ccdabb3143496f1d4de9eb1cb054073f7
Brady Vercher <brady@blazersix.com>
2016-11-25 23:18:52 -0800

    Stop setting the width of the search field in the internal link dialog.

--- js/internal-link-manager.js
+++ js/internal-link-manager.js
@@ -80,7 +80,7 @@
 		 * Initialize the wpLink manager.
 		 */
 		init: function() {
-			this.$searchField = $( '#search-field, #wp-link-search' ).width( 200 );
+			this.$searchField = $( '#search-field, #wp-link-search' );
 
 			// Proxy the internal link search method.
 			this._wpLinkSearchInternalLinks = wpLink.searchInternalLinks;
